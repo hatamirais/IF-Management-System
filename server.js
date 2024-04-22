4ebd74aa9e09a46b905956670fe2b0218578fcbd
Hatami Ra'is Bukhari <hatamierbe12@gmail.com>
2024-04-22 14:21:55 +0700

    commit

--- server.js
+++ server.js
@@ -1,23 +1,63 @@
-// Source -> https://heynode.com/tutorial/process-user-login-form-expressjs/
-
+// source : https://codeshack.io/basic-login-system-nodejs-express-mysql/
+const mysql = require('mysql')
 const express = require('express')
+const session = require('express-session')
+const path = require('path')
+
+const connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'if-management-system'
+})
+
 const app = express()
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended:false }))
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/index.html')
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+}))
+app.use(express.json)
+app.use(express.urlencoded({ extended : true }))
+app.use(express.static(path.join(__dirname, 'static')))
+
+
+app.get('/', function(request, response){
+    response.sendFile(path.join(__dirname+'/index.html'))
 })
 
-app.get('/success', (req, res) => {
-    res.sendFile(__dirname+'/page/success.html')
+app.post('/auth', function(request, response){
+    const username = request.body.username
+    const password = request.body.password
+
+    if (username && password){
+        connection.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function(error, results, fields){
+            if (error) throw error
+            if (results.length > 0){
+                request.session.loggedin = true
+                request.session.username = username
+
+                response.redirect('/home')
+            } else {
+                response.send('Incorrect username and/or password')
+            }
+            response.end()
+        })
+    } else {
+        response.send('please enter username and password')
+        response.end()
+    }
 })
 
-app.post('/', (req, res) => {
-    const username = req.body.username
-    const password = req.body.password
-    res.send(`Username: ${username} Password: ${password}`)
+app.get('/home', function(request, response){
+    if (request.session.loggedin){
+        response.sendFile(path.join(__dirname+'/page/success.html'))
+    } else {
+        response.send('Please login to view this page')
+    }
+    response.end()
 })
 
 const port = 3000
-app.listen(port, () => console.log(`App is running on port ${port}`))
+app.listen(port, () => console.log(`App is listening on port ${port}`))
